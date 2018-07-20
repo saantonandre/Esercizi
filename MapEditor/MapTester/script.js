9a87cb8d2ff01aaeb4a2ee4ce94ffff91dd06611
Andrea <andreasantonandreasantona.gi@gmail.com>
2018-07-20 14:12:16 +0200

    sprite update

--- MapEditor/MapTester/script.js
+++ MapEditor/MapTester/script.js
@@ -3,9 +3,14 @@ window.onload = function () {
         return document.getElementById(arg);
     }
     var canvas = id("canvas");
-
+    canvas.imageSmoothingEnabled = false;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    var CHARA_IDLE = id("CHARA_IDLE");
+    var CHARA_WALK_L = id("CHARA_WALK_L");
+    var CHARA_WALK_R = id("CHARA_WALK_R");
+    var CHARA_WALK_T = id("CHARA_WALK_T");
+    var CHARA_WALK_B = id("CHARA_WALK_B");
     var c = canvas.getContext("2d");
     var cellSize = 30;
     var player = {
@@ -21,7 +26,22 @@ window.onload = function () {
         xVel: 0,
         yVel: 0,
         speed: 3,
-        accel: 0.25
+        accel: 0.25,
+        /* sprites have an address and a delay */
+        sprites: [
+            /* IDLE: */
+            [CHARA_IDLE, 10],
+            /* L: */
+            [CHARA_WALK_L, 4],
+            /* R: */
+            [CHARA_WALK_R, 4],
+            /* T: */
+            [CHARA_WALK_T, 7],
+            /* B: */
+            [CHARA_WALK_B, 7]
+        ],
+        currentSprite: 0,
+        currentFrame: 0
     };
 
     var map = [];
@@ -49,9 +69,11 @@ window.onload = function () {
 
     function loop() {
         c.clearRect(0, 0, canvas.width, canvas.height);
+
         drawPlayer();
         drawMap();
         calculatePlayer();
+
         requestAnimationFrame(loop);
     }
 
@@ -99,17 +121,48 @@ window.onload = function () {
         }
         return false;
     }
+    var frameCounter = 0;
 
     function drawPlayer() {
         c.fillStyle = "#00ff00";
-        c.fillRect(
+
+        /* if the player is not moving he is idling */
+
+        if (!player.L &&
+            !player.R &&
+            !player.T &&
+            !player.B) {
+            player.currentSprite = 0;
+        }
+
+        /* going down through the sprite frame */
+
+        frameCounter++;
+
+        /* if the next frame does not exist return to 0 */
+        if (player.currentFrame + 1 > player.sprites[player.currentSprite][0].height / player.sprites[player.currentSprite][0].width / 2) {
+            frameCounter = 0;
+        }
+        /* currentFrame è l'indice dell'altezza della sprite */
+        player.currentFrame = parseInt(frameCounter / player.sprites[player.currentSprite][1]);
+        //console.log(frameCounter)
+
+        c.drawImage(
+            player.sprites[player.currentSprite][0],
+            0,
+            64 * player.currentFrame,
+            32,
+            64,
             player.x * cellSize,
             player.y * cellSize,
             player.w * cellSize,
             player.h * cellSize
         );
+
+
         c.stroke();
         c.beginPath();
+
         c.strokeStyle = "#ff0000";
         c.rect(
             player.x * cellSize,
@@ -120,6 +173,7 @@ window.onload = function () {
         c.closePath();
         c.stroke();
     }
+    /* SPRITE RENDERING */
 
     function calculatePlayer() {
         playerHitbox = {
@@ -128,15 +182,18 @@ window.onload = function () {
             w: player.w,
             h: player.h - 1
         }
+
         directionCheck();
 
         isGrounded(playerHitbox);
+
         mapX -= player.xVel;
         mapY -= player.yVel;
     }
 
 
     function directionCheck() {
+
         if (player.L || player.R) {
             if (player.R && player.xVel < player.speed) {
                 player.xVel += player.accel;
@@ -169,7 +226,9 @@ window.onload = function () {
 
     function isGrounded(entity) {
         id("stat").innerHTML = "";
+
         for (i = 0; i < hitBoxes.length; i++) {
+
             var col = collision(entity, hitBoxes[i]);
             if (col.l) {
                 id("stat").innerHTML += "L ";
@@ -190,15 +249,8 @@ window.onload = function () {
             }
         }
 
+
     }
-
-
-
-
-
-
-
-
 
     //COLLISION DETECTOR
     function collision(Box1, Box2) {
@@ -240,21 +292,24 @@ window.onload = function () {
 
 
 
-
     window.addEventListener("keydown", function (event) {
         var key = event.keyCode;
         switch (key) {
             case 65: //left key down
                 player.L = true;
+                player.currentSprite = 1;
                 break;
             case 68: //right key down
                 player.R = true;
+                player.currentSprite = 2;
                 break;
             case 87: //top key down
                 player.T = true;
+                player.currentSprite = 3;
                 break;
             case 83: //bot key down
                 player.B = true;
+                player.currentSprite = 4;
                 break;
         }
     });
