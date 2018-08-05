e1e07b88b5bf355fa324f9784548c48b435efab4
Andrea <andreasantonandreasantona.gi@gmail.com>
2018-08-05 06:17:41 +0200

    .

--- DialogueEditor/script.js
+++ DialogueEditor/script.js
@@ -15,7 +15,15 @@ pageBtns = document.getElementsByClassName("page");
 pageBtns[pages].addEventListener("click", function () {
     switchPage(parseInt(this.id))
 });
+id("test").onclick = function () {
+    id("editor").style.display = "none";
+    id("tester").style.display = "block";
+}
 
+id("edit").onclick = function () {
+    id("tester").style.display = "none";
+    id("editor").style.display = "block";
+}
 
 
 
@@ -34,7 +42,7 @@ id("new-page").onclick = function () {
         trigger: ""
     }
     pageBtns[pages].addEventListener("click", function () {
-        switchPage(parseInt(this.id))
+        switchPage(parseInt(this.id));
     });
 
 }
@@ -46,7 +54,6 @@ id("new-page").onclick = function () {
 function switchPage(pageId) {
     id(pageId).classList += " selected";
     id(currentPage).classList.remove("selected");
-    console.log(dialogue[pageId]);
 
     dialogue.splice(currentPage, 1, {
         condition: id("text1").value,
@@ -62,17 +69,107 @@ function switchPage(pageId) {
     currentPage = pageId;
 }
 
+function save() {
+    dialogue.splice(currentPage, 1, {
+        condition: id("text1").value,
+        option: id("text2").value,
+        text: id("text3").value,
+        trigger: id("text4").value
+    });
+}
 
-
-
-function exportDialogue() {
-    dialogue = [];
-    for (i = 0; i < pages.length; i++) {
-        dialogue.push({
-            condition: id("text1").value,
-            option: id("text2").value,
-            text: id("text3").value,
-            trigger: id("text4").value
-        })
+id("export").onclick = function () {
+    save();
+    var output = "dialogue=[";
+    for (var i = 0; i < dialogue.length; i++) {
+        output += "{condition: \"" + dialogue[i].condition + "\", option: \"" + dialogue[i].option + "\", text: \"" + dialogue[i].text + "\", trigger: \"" + dialogue[i].trigger + "\"}";
+        if (!(i + 1 === dialogue.length))
+            output += ",";
     }
+    output += "]";
+    prompt("Copy this dialogue code and save it wherever possible", output);
+}
+
+id("import").onclick = function () {
+    eval(prompt("Insert the dialogue code here", ""));
+    restock();
+}
+
+function restock() {
+    pages = 0;
+    for (var i = pageBtns.length - 1; i >= 0; i--) {
+        pageBtns[i].parentNode.removeChild(pageBtns[i]);
+    }
+    for (var i = 0; i < dialogue.length; i++) {
+        var newNode = document.createElement("BUTTON");
+        var textNode = document.createTextNode(i);
+        newNode.appendChild(textNode);
+        newNode.className = "page";
+        newNode.id = i;
+        id("new-page").parentNode.insertBefore(newNode, id("new-page"));
+
+        pageBtns[i].addEventListener("click", function () {
+            switchPage(parseInt(this.id));
+        });
+        pages = i;
+    }
+
+    currentPage = 0;
+    id("text1").value = dialogue[0].condition;
+    id("text2").value = dialogue[0].option;
+    id("text3").value = dialogue[0].text;
+    id("text4").value = dialogue[0].trigger;
+
+    id("0").classList.remove("selected");
+    id("0").classList += " selected";
+}
+
+
+
+
+
+
+
+
+
+// DIALOGUE TESTING
+var optionList = [];
+id("testStart").onclick = function () {
+    istanceVariables();
+    validateOptions();
+    generateOptions();
+};
+
+function istanceVariables() {
+    eval(id("istances").value);
+}
+
+function validateOptions() {
+    optionList = [];
+    for (var i = 0; i < dialogue.length; i++) {
+        if (eval(dialogue[i].condition) || eval(dialogue[i].condition.length == 0)) {
+            optionList.push(i);
+        }
+    }
+}
+
+function generateOptions() {
+    id("options").innerHTML = "";
+    for (var i = 0; i < optionList.length; i++) {
+        var newNode = document.createElement("BUTTON");
+        var textNode = document.createTextNode(dialogue[optionList[i]].option);
+
+        newNode.appendChild(textNode);
+        newNode.className = "option";
+        newNode.id = "option-" + i;
+        newNode.number = optionList[i];
+        id("options").appendChild(newNode);
+        newNode.addEventListener("click", function () {
+            id("output").innerHTML = dialogue[this.number].text;
+            eval(dialogue[this.number].trigger);
+            validateOptions();
+            generateOptions();
+        });
+    }
+
 }
