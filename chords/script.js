2dc5ba56ab22afbb8447355ca09e637dd2ef97b7
saantonandre <36368530+saantonandre@users.noreply.github.com>
2020-01-03 17:15:17 +0100

    ,

--- chords/script.js
+++ chords/script.js
@@ -1,13 +1,16 @@
 function id(arg) {
     return document.getElementById(arg);
 }
-
+var tempo = -id("tempo").value * 10;
 var notes = [130.8, 138.6, 146.8, 155.6, 164.8, 174.6, 185.0, 196.0, 207.7, 220.0, 233.1, 246.9, 261.6, 277.2, 293.7, 311.1, 329.6, 349.2, 370.0, 392.0, 415.3, 440.0, 466.2, 493.9, 523.3, 554.4, 587.3, 622.3, 659.3, 698.5, 740.0, 784.0, 830.6, 880.0, 932.3, 987.8, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902];
 id("tempo").onchange = function () {
-    console.log(-this.value)
+    tempo = -id("tempo").value * 10;
 }
 id("arpSpeed").onchange = function () {
     arpSpeed = -id("arpSpeed").value;
+}
+id("volume").onchange = function () {
+    volume = -id("volume").value * 0.01;
 }
 
 var arpSpeed = -id("arpSpeed").value;
@@ -111,7 +114,7 @@ function playChord(note, type) {
                 }, arpSpeed)
                 setTimeout(function () {
                     playNote(notes[note.i + 7]);
-                }, arpSpeed*2)
+                }, arpSpeed * 2)
             } else {
                 playNote(notes[note.i + 4]);
                 playNote(notes[note.i + 7]);
@@ -124,7 +127,7 @@ function playChord(note, type) {
                 }, arpSpeed)
                 setTimeout(function () {
                     playNote(notes[note.i + 7]);
-                }, arpSpeed*2)
+                }, arpSpeed * 2)
             } else {
                 playNote(notes[note.i + 3]);
                 playNote(notes[note.i + 7]);
@@ -137,10 +140,10 @@ function playChord(note, type) {
                 }, arpSpeed)
                 setTimeout(function () {
                     playNote(notes[note.i + 7]);
-                }, arpSpeed*2)
+                }, arpSpeed * 2)
                 setTimeout(function () {
                     playNote(notes[note.i + 10]);
-                }, arpSpeed*3)
+                }, arpSpeed * 3)
             } else {
                 playNote(notes[note.i + 4]);
                 playNote(notes[note.i + 7]);
@@ -150,35 +153,55 @@ function playChord(note, type) {
     }
 }
 var contextes = [];
+var volume = -id("volume").value * 0.01;
 
-function playNote(frequency) {
+function playNote(frequency, flag) {
     let context = new AudioContext();
     let o = context.createOscillator();
     let g = context.createGain();
-    o.connect(g);
-    g.connect(context.destination);
-    console.log(frequency);
-    o.frequency.value = frequency;
-    o.start(0)
-    g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 3);
-    contextes.push(context);
-    if (contextes.length >= 6) {
-        contextes[contextes.length - 6].close();
-    }
-}
+    let freq = frequency;
 
+    if (flag) {
+        for (let i = 0; i < notes.length; i++) {
+            if (freq == notes[i]) {
+                freq = notes[i + 24];
+                break;
+            }
+        }
+    }
+
+    g.connect(context.destination);
+    //console.log(frequency);
+    o.frequency.value = freq;
+    g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 2);
+
+    o.connect(g);
+    contextes.push(context);
+    if (contextes.length >= 12) {
+        contextes[contextes.length - 12].close();
+    }
+
+    o.start(volume)
+}
+id("melody").onclick = function () {
+    playMelody(notesInChords);
+}
+var notesInChords;
 var ST = setTimeout(function () {}, 1000);
 var btn1 = id("generate");
 btn1.onclick = function () {
     clearTimeout(ST);
     var chords = generateChords();
+    notesInChords = findNotes(chords);
+    
+    playMelody(notesInChords);
     for (let i = 0; i < chords.length; i++) {
         id("" + i).innerHTML = chords[i].note + " " + chords[i].type;
     }
     var iter = 0;
 
     function x() {
-        ST = setTimeout(x, -tempo.value * 10);
+        ST = setTimeout(x, tempo);
         switchChord(chords, iter);
         iter++;
         if (iter >= chords.length) {
@@ -188,6 +211,80 @@ btn1.onclick = function () {
     x();
 
 
+}
+
+function playMelody(notesInChords) {
+    let nIC = notesInChords;
+    playNote(nIC[Math.random() * nIC.length | 0], 1);
+    setTimeout(function () {
+        playMelody(notesInChords)
+    }, tempo / (Math.random() * 16 | 0 + 1) * 2);
+    console.log(nIC)
+}
+
+function findNotes(chords) {
+    let notesList = [];
+    let note;
+    for (let i = 0; i < chords.length; i++) {
+        switch (chords[i].note) {
+            case "DO":
+                note = DO[1];
+                break;
+            case "DO#":
+                note = DO1[1];
+                break;
+            case "RE":
+                note = RE[1];
+                break;
+            case "RE#":
+                note = RE1[1];
+                break;
+            case "MI":
+                note = MI[1];
+                break;
+            case "FA":
+                note = FA[1];
+                break;
+            case "FA#":
+                note = FA1[1];
+                break;
+            case "SOL":
+                note = SOL[1];
+                break;
+            case "SOL#":
+                note = SOL1[1];
+                break;
+            case "LA":
+                note = LA[1];
+                break;
+            case "LA#":
+                note = LA1[1];
+                break;
+            case "SI":
+                note = SI[1];
+                break;
+        }
+        switch (chords[i].type) {
+            case "mag":
+                notesList.push(notes[note.i], notes[note.i + 4], notes[note.i + 7])
+                break;
+            case "min":
+                notesList.push(notes[note.i], notes[note.i + 3], notes[note.i + 7])
+                break;
+            case "set":
+                notesList.push(notes[note.i], notes[note.i + 4], notes[note.i + 7], notes[note.i + 10])
+                break;
+        }
+    }
+    for (let i = 0; i < notesList.length; i++) {
+        for (let j = i + 1; j < notesList.length; j++) {
+            if (notesList[i] == notesList[j]) {
+                notesList.splice(j);
+                j--;
+            }
+        }
+    }
+    return notesList;
 }
 
 function switchChord(chords, iter) {
